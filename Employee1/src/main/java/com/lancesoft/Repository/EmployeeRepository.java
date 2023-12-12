570f401299d2c20dc3cd16241915b155ab28b791
Tejashwini-Akuthota <56907407+Tejashwini-Akuthota@users.noreply.github.com>
2023-12-12 12:37:49 +0530

    Delete Employee1 directory

--- Employee1/src/main/java/com/lancesoft/Repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lancesoft.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lancesoft.Entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
